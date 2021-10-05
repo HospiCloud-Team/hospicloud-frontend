2f7ec03d347be2a55e43e71f91895d060f4f11e1
kelvinlee0225 <leekelvin0225@gmail.com>
2021-10-05 01:19:28 -0400

    Add buttons, labels to Login Page

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,5 +1,35 @@
+import LandingLayout from "../../layout/LandingLayout";
+
 const LoginPage = () => {
-  return <div>Login</div>;
+  
+  return (
+    <LandingLayout>
+      <h1 className="fs-2">Login</h1>
+      <form>
+        <div className="mb-3">
+          <label id="labelEmail" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="inputEmail"/>
+          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div className="mb-3">
+          <label id="exampleInputPassword1" className="form-label">Password</label>
+          <input type="password" className="form-control" id="inputPassword"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+    <form>
+      <div className="mb-3">
+        <label>Email address</label>
+        <input type="email"/>
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password"/>
+      </div>
+      <button type="submit" className="btn btn-primary">Login</button>
+    </form>
+    </LandingLayout>
+  );
 };
 
 export default LoginPage;
