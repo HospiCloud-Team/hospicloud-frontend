bf31da4b56ad00daab308a91e5ea06479b34c380
kelvinlee0225 <leekelvin0225@gmail.com>
2021-10-12 01:19:45 -0400

    Add Login Page Design

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,33 +1,42 @@
 import LandingLayout from "../../layout/LandingLayout";
+import { MultiBg, FixedBox, Icon, ContainerDiv, LoginTitle} from "./style/index.style";
+import HospiCloudLogo from '../../resources/HospiCloudLogo.svg'
 
 const LoginPage = () => {
   
   return (
     <LandingLayout>
-      <h1 className="fs-2">Login</h1>
-      <form>
-        <div className="mb-3">
-          <label id="labelEmail" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="inputEmail"/>
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div className="mb-3">
-          <label id="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="inputPassword"/>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
-    <form>
-      <div className="mb-3">
-        <label>Email address</label>
-        <input type="email"/>
-      </div>
-      <div>
-        <label>Password</label>
-        <input type="password"/>
-      </div>
-      <button type="submit" className="btn btn-primary">Login</button>
-    </form>
+      <MultiBg>
+        <FixedBox>
+                <div className="card-block my-5 py-2">
+                  <ContainerDiv>
+                    <div className="row my-auto">
+                      <div className="col d-block">
+                        <Icon className="mx-auto" src={HospiCloudLogo} alt="Logo"/>
+                      </div>
+                      <div className="col d-block m-3">
+                        <LoginTitle className="d-flex justify-content-start mb-3">Login</LoginTitle>
+                        <form>
+                          <div className="form-group mb-2">
+                            <input type="text" className="form-control" placeholder="Username"/>
+                            </div>
+                            <div className="form-group mb-4">
+                                <input type="password" className="form-control" placeholder="Password" />
+                            </div>
+                            <div className="form-group d-flex justify-content-between mb-3">
+                                <a href="/ForgetPassword">Olvidaste tu contraseña</a>
+                                <button type="submit" className="btn btn-primary ">Iniciar Sesion</button>
+                            </div>
+                            <div className="form-group d-flex justify-content-end">
+                                <a href="/Register">Register</a>
+                            </div>
+                        </form>
+                      </div>
+                    </div>
+                </ContainerDiv>
+              </div>
+        </FixedBox>
+      </MultiBg>
     </LandingLayout>
   );
 };
