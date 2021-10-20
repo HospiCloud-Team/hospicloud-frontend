5b5f2f953d6b249092e0a49d876b49a0a190a2b7
kelvinlee0225 <leekelvin0225@gmail.com>
2021-10-20 01:34:39 -0400

    Add RegisterPage

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import routes from "./constantRoutes.json";
 import LandingPage from "../pages/LandingPage";
 import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
 import HospitalDetail from "../pages/HospitalDetail";
 
 const Routes = () => {
@@ -12,6 +13,9 @@ const Routes = () => {
       </Route>
       <Route path={routes.LOGIN}>
         <LoginPage />
+      </Route>
+      <Route path={routes.REGISTER}>
+        <RegisterPage />
       </Route>
       <Route path={`${routes.HOSPITALS}/:id`}>
         <HospitalDetail />
