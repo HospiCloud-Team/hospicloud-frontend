7a9063f3892a10afc4f25185c49319f9275748b4
kelvinlee0225 <leekelvin0225@gmail.com>
2021-11-03 21:08:12 -0400

    Update IHospital2 interface

--- src/models/IHospital2.ts
+++ src/models/IHospital2.ts
@@ -1,9 +1,16 @@
 export interface IHospital2 {
+  id?: number;
     name: string;
     schedule: string;
-    location: {
-      address: string;
-      province: string;
-    };
+    location: ILocation;
   }
+
+export interface ILocation {
+  address: string;
+  province: string;
+}
   
+  export type ContextType = {
+    hospitalData: IHospital2
+    saveHospitalData: (HospitalData: IHospital2) => void
+  }
