0e167042e00fd38f227f7c6b5319af1c1300e843
kelvinlee0225 <leekelvin0225@gmail.com>
2021-11-21 17:36:17 -0400

    AB-117: add IDoctorRegister and Specialtiesjson

--- src/models/IDoctor.ts
+++ src/models/IDoctor.ts
@@ -13,3 +13,18 @@ export interface IDoctor {
     id: number;
   };
 }
+
+export interface IDoctorRegister {
+  user_role: "doctor";
+  document_type: string;
+  name: string;
+  last_name: string;
+  email: string;
+  document_number: string;
+  date_of_birth: Date;
+  doctor: {
+    hospital_id: number;
+    schedule: string;
+    specialty_ids: number[];
+  };
+}
