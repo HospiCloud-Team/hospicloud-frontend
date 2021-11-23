7b1ec0f65c825215b24a5acdc5ab721f037788c3
kelvinlee0225 <leekelvin0225@gmail.com>
2021-11-22 22:32:55 -0400

    AB-117: add two buttons for conditional rendering

--- src/pages/DashboardAdmin/components/PersonnelList.tsx
+++ src/pages/DashboardAdmin/components/PersonnelList.tsx
@@ -13,11 +13,22 @@ import {
   Border,
 } from "../styles/PersonnelList.style";
 import routes from "../../../router/constantRoutes.json";
-
 const PersonnelList = () => {
   const [admins, setAdmins] = useState<IAdmin[]>([]);
   const [doctors, setDoctors] = useState<IDoctor[]>([]);
+  const [isAdminList, setIsAdminList] = useState<boolean>(true);
+  const [isDocotrList, setIsDoctorList] = useState<boolean>(false);
   let history = useHistory();
+
+  const handleAdminListClick = () => {
+    setIsDoctorList(false);
+    setIsAdminList(true);
+  };
+
+  const handleDoctorListClick = () => {
+    setIsAdminList(false);
+    setIsDoctorList(true);
+  };
 
   useEffect(() => {
     const getAll = async () => {
@@ -33,76 +44,88 @@ const PersonnelList = () => {
 
   return (
     <div>
-      <PersonnelContainer>
-        <div className="d-flex flex-column w-100">
-          <div className="d-flex flex-row align-items-center">
-            <Title>Admins</Title>
-            <div className="w-100 d-flex justify-content-end">
-              <AddButton
-                type="button"
-                className="btn btn-primary btn-lg"
-                onClick={() => {
-                  history.push(routes.REGISTER_ADMIN_PERSONNEL);
-                }}
-              >
-                <div className="buttonText">+</div>
-                <div className="TextAfterHover">+ Agregar Admin</div>
-              </AddButton>
+      <div className="d-flex flex-row w-100 justify-space-between">
+        <button className="btn btn-secondary" onClick={handleAdminListClick}>
+          Administradores
+        </button>
+        <button className="btn btn-secondary" onClick={handleDoctorListClick}>
+          Doctores
+        </button>
+      </div>
+      {isAdminList && (
+        <PersonnelContainer>
+          <div className="d-flex flex-column w-100">
+            <div className="d-flex flex-row align-items-center">
+              <Title>Admins</Title>
+              <div className="w-100 d-flex justify-content-end">
+                <AddButton
+                  type="button"
+                  className="btn btn-primary btn-lg"
+                  onClick={() => {
+                    history.push(routes.REGISTER_ADMIN_PERSONNEL);
+                  }}
+                >
+                  <div className="buttonText">+</div>
+                  <div className="TextAfterHover">+ Agregar Admin</div>
+                </AddButton>
+              </div>
             </div>
+            <Border />
           </div>
-          <Border />
-        </div>
-        {admins.map((admin) => {
-          return (
-            <div className="row bg-white mx-0 my-3 p-3 rounded-2 position-relative">
-              <div className="col-1 border-end">
-                <button className="btn btn-primary btn-sm stretched-link">
-                  <i className="bi bi-person-badge"></i>
-                </button>
+          {admins.map((admin) => {
+            return (
+              <div className="row bg-white mx-0 my-3 p-3 rounded-2 position-relative">
+                <div className="col-1 border-end">
+                  <button className="btn btn-primary btn-sm stretched-link">
+                    <i className="bi bi-person-badge"></i>
+                  </button>
+                </div>
+                <div className="col-11 d-flex justify-content-between">
+                  <p className="m-0">{admin.name}</p>
+                  <p className="m-0">{admin.email}</p>
+                </div>
               </div>
-              <div className="col-11 d-flex justify-content-between">
-                <p className="m-0">{admin.name}</p>
-                <p className="m-0">{admin.email}</p>
+            );
+          })}
+        </PersonnelContainer>
+      )}
+      {isDocotrList && (
+        <PersonnelContainer>
+          <div className="d-flex flex-column w-100">
+            <div className="d-flex flex-row align-items-center">
+              <Title>Doctors</Title>
+              <div className="w-100 d-flex justify-content-end">
+                <AddButton
+                  type="button"
+                  className="btn btn-primary btn-lg"
+                  onClick={() => {
+                    history.push(routes.REGISTER_DOCTOR_PERSONNEL);
+                  }}
+                >
+                  <div className="buttonText">+</div>
+                  <div className="TextAfterHover">+ Agregar Doctor</div>
+                </AddButton>
               </div>
             </div>
-          );
-        })}
-      </PersonnelContainer>
-      <PersonnelContainer>
-        <div className="d-flex flex-column w-100">
-          <div className="d-flex flex-row align-items-center">
-            <Title>Doctors</Title>
-            <div className="w-100 d-flex justify-content-end">
-              <AddButton
-                type="button"
-                className="btn btn-primary btn-lg"
-                onClick={() => {
-                  history.push(routes.REGISTER_DOCTOR_PERSONNEL);
-                }}
-              >
-                <div className="buttonText">+</div>
-                <div className="TextAfterHover">+ Agregar Doctor</div>
-              </AddButton>
-            </div>
+            <Border />
           </div>
-          <Border />
-        </div>
-        {doctors.map((doctor) => {
-          return (
-            <div className="row bg-white mx-0 my-3 p-3 rounded-2 position-relative">
-              <div className="col-1 border-end">
-                <button className="btn btn-primary btn-sm stretched-link">
-                  <i className="bi bi-person-badge"></i>
-                </button>
+          {doctors.map((doctor) => {
+            return (
+              <div className="row bg-white mx-0 my-3 p-3 rounded-2 position-relative">
+                <div className="col-1 border-end">
+                  <button className="btn btn-primary btn-sm stretched-link">
+                    <i className="bi bi-person-badge"></i>
+                  </button>
+                </div>
+                <div className="col-11 d-flex justify-content-between">
+                  <p className="m-0">{doctor.name}</p>
+                  <p className="m-0">{doctor.email}</p>
+                </div>
               </div>
-              <div className="col-11 d-flex justify-content-between">
-                <p className="m-0">{doctor.name}</p>
-                <p className="m-0">{doctor.email}</p>
-              </div>
-            </div>
-          );
-        })}
-      </PersonnelContainer>
+            );
+          })}
+        </PersonnelContainer>
+      )}
     </div>
   );
 };
