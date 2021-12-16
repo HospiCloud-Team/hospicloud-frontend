7dd24558720d1b943983b43d78bde12989911062
kelvinlee0225 <leekelvin0225@gmail.com>
2021-12-15 23:28:51 -0400

    AB-130: add onClick method in CheckupItem

--- src/components/CheckupItem.tsx
+++ src/components/CheckupItem.tsx
@@ -1,27 +1,25 @@
-import { useHistory } from "react-router";
 import { ICheckup } from "../models/ICheckup";
 
 interface CheckupItemProps {
-  name: string;
-  route: string;
+  patientName: string;
   checkup: ICheckup;
+  onClick?: () => void;
 }
 
-const CheckupItem = ({ name, route, checkup }: CheckupItemProps) => {
-  const history = useHistory();
-
+const CheckupItem = ({ patientName, checkup, onClick }: CheckupItemProps) => {
   return (
     <div className="row bg-white mx-0 my-3 p-3 rounded-2 position-relative">
       <div className="col-1 border-end">
         <button
           className="btn btn-primary btn-sm stretched-link"
-          onClick={() => history.push(route, checkup)}
+          onClick={onClick}
         >
           <i className="bi bi-journal-text"></i>
         </button>
       </div>
       <div className="col-11 d-flex justify-content-between">
-        <p className="m-0">{name}</p>
+        <p className="m-0">{patientName}</p>
+        <p className="m-0 fw-bold">By Dr. Cesar Lopez</p>
         <p className="m-0">{new Date(checkup.date).toLocaleString("es-DO")}</p>
       </div>
     </div>
